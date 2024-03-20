21a586347f355daa41bde8bccd090cd855237677
GithinjiKelvin <githinjikelvin59@gmail.com>
2024-03-20 22:39:17 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let multiply 
+let num1 = 2
+let num2 = 31
+multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let mod
+let num3 = 9
+let num4 = 5
+mod = num3 % num4
+
+let max = Math.max(3, 5,20, 8, 9)
+
